e6f6fe91c49614ef6da8aa4a2bf9d5ba6ac4ab02
Stenael <stevenputranatanael@gmail.com>
2023-05-17 15:14:20 +0700

    init

--- app/src/main/java/com/example/todokpb/view/MainActivity.kt
+++ app/src/main/java/com/example/todokpb/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.todokpb
+package com.example.todokpb.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todokpb.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
